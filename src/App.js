0911f0a1d59958ec0e608523cca3917dee729eef
zohaibashrafdevops <zohaib.ashraf@eurustechnologies.com>
2022-08-02 03:03:32 +0500

    upate

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Lear
+                    Learn React
                 </a>
             </header>
         </div>
